dba43d39401d43cb48765a7ead00fd117858aaf3
nadia <nadia20ti@mahasiswa.pcr.ac.id>
2021-07-02 16:51:50 +0700

    Menambahkan pesan output hello world

--- BelajarGit/JavaAp/src/javaap/JavaAp.java
+++ BelajarGit/JavaAp/src/javaap/JavaAp.java
@@ -15,7 +15,8 @@ public class JavaAp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello Word");
+        System.out.println("Hai");
     }
     
 }
