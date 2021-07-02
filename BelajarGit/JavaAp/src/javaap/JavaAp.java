c0bbacf1711df5a9da46e3280868546d36115dd4
nadia <nadia20ti@mahasiswa.pcr.ac.id>
2021-07-02 17:09:34 +0700

    Menambahkan method kurang

--- BelajarGit/JavaAp/src/javaap/JavaAp.java
+++ BelajarGit/JavaAp/src/javaap/JavaAp.java
@@ -11,12 +11,13 @@ package javaap;
  */
 public class JavaAp {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static int kurang(int a, int b){
+        return a-b;
+    }
     public static void main(String[] args) {
         System.out.println("Hello Word");
         System.out.println("Hai");
+        System.out.println(kurang(3,2));
     }
     
 }
